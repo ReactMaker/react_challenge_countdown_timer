d4b2d7b7e4a897d2321f346e78c584314a1ce507
chnbohwr <chnbohwr@gmail.com>
2017-06-25 10:33:40 +0800

    add countdown by momentjs

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
-
+import moment from 'moment';
 import './Home.less';
 
 export default class Home extends Component {
+
+  state = {
+    remindTime: 0,
+    endDate: moment()
+  }
+
+  interval = undefined;
+
+  countDown = (second) => {
+    this.setState({
+      remindTime: second,
+      endDate: moment().add(second, 'second'),
+    });
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      this.setState({
+        remindTime: this.state.remindTime -= 1,
+      });
+      if (this.state.remindTime <= 0) {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  enterMinutes = (e) => {
+    const minutes = parseInt(e.target.value, 10) * 60;
+    if (e.key === 'Enter' && minutes) {
+      this.countDown(minutes);
+    }
+  }
+
+  showTime = () => {
+    const timer = moment()
+      .startOf('day')
+      .add(this.state.remindTime, 'second');
+    if (this.state.remindTime >= 3600) {
+      return timer.format('HH:mm:ss');
+    }
+    return timer.format('mm:ss');
+  }
+
+  showEndTime = () => this.state.endDate.format('HH:mm:ss');
+
+
   render() {
     return (
       <div id="pageHome">
         <div className="timerControls">
-          <button data-time="20" className="timerButton">20 Secs</button>
-          <button data-time="300" className="timerButton">Work 5</button>
-          <button data-time="900" className="timerButton">Quick 15</button>
-          <button data-time="1200" className="timerButton">Snack 20</button>
-          <button data-time="3600" className="timerButton">Lunch Break</button>
-          <input type="text" name="minutes" placeholder="Enter Minutes" />
+          <button className="timerButton" onClick={() => { this.countDown(20); }}>20 Secs</button>
+          <button className="timerButton" onClick={() => { this.countDown(300); }}>Work 5</button>
+          <button className="timerButton" onClick={() => { this.countDown(900); }}>Quick 15</button>
+          <button className="timerButton" onClick={() => { this.countDown(1200); }}>Snack 20</button>
+          <button className="timerButton" onClick={() => { this.countDown(3600); }}>Lunch Break</button>
+          <input type="number" min="0" max="999" placeholder="Enter Minutes" onKeyPress={this.enterMinutes} />
         </div>
         <div className="display">
-          <h1 className="displayTL">asdf</h1>
-          <p className="displayTR">wefr</p>
+          <h1 className="displayTL">{this.showTime()}</h1>
+          <p className="displayTR">Be Back At {this.showEndTime()}</p>
         </div>
       </div>
     );
