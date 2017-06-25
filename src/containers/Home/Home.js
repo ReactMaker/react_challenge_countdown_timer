37331168d95833de7a94ddddb4f191e3d7c675b1
chnbohwr <chnbohwr@gmail.com>
2017-06-25 09:42:55 +0800

    add html and css

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,7 +6,18 @@ export default class Home extends Component {
   render() {
     return (
       <div id="pageHome">
-        this is home page
+        <div className="timerControls">
+          <button data-time="20" className="timerButton">20 Secs</button>
+          <button data-time="300" className="timerButton">Work 5</button>
+          <button data-time="900" className="timerButton">Quick 15</button>
+          <button data-time="1200" className="timerButton">Snack 20</button>
+          <button data-time="3600" className="timerButton">Lunch Break</button>
+          <input type="text" name="minutes" placeholder="Enter Minutes" />
+        </div>
+        <div className="display">
+          <h1 className="displayTL">asdf</h1>
+          <p className="displayTR">wefr</p>
+        </div>
       </div>
     );
   }
